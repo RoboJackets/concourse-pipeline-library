deb828169f4de48614e24d7914b76585657c4d3e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-16 18:30:22 +0800

    Delete tools directory

--- tools/parse_filename.py
+++
@@ -1,6 +0,0 @@
-#/bin/python3
-
-from re import fullmatch
-from sys import stdin, argv
-
-print(fullmatch(r"pipelines/(?P<team>[a-z-]+)/(?P<pipeline>[a-z-]+).yml", stdin.read().strip()).group(argv[1]))
